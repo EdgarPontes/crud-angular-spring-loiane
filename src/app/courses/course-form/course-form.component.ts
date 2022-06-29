cc3ea34d475538b403e8446f4a8834bde923603b
Edgar <edgarponte@gmail.com>
2022-06-29 15:51:48 -0300

    Aula 19 - Formulário para Criar Cursos

--- src/app/courses/course-form/course-form.component.ts
+++ src/app/courses/course-form/course-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-course-form',
@@ -7,7 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CourseFormComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup;
+
+  constructor( private formBuilder: FormBuilder ) {
+    this.form = this.formBuilder.group({
+      name: [null],
+      category: [null]
+    });
+  }
+
+  onSubmit() {
+
+  }
+
+  onCancel() {
+
+  }
 
   ngOnInit(): void {
   }
