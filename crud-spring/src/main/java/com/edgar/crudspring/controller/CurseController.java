c5194b18c8a2a9bc9b366a342a4b6e70fca362db
Edgar <edgarponte@gmail.com>
2022-06-30 12:51:45 -0300

    Aula - 21: Formulário: API Spring: Criar Curso (HTTP POST)

--- crud-spring/src/main/java/com/edgar/crudspring/controller/CurseController.java
+++ crud-spring/src/main/java/com/edgar/crudspring/controller/CurseController.java
@@ -3,8 +3,13 @@ package com.edgar.crudspring.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.edgar.crudspring.model.Course;
@@ -23,6 +28,16 @@ public class CurseController {
     @GetMapping
     public List<Course> list(){
         return courseRepository.findAll();
+    }
+
+    //@RequestMapping(method: RequestMethod.POST)
+    @PostMapping
+    @ResponseStatus(code = HttpStatus.CREATED)
+    public Course create (@RequestBody Course course) {
+        System.out.println(course.getName());
+        System.out.println(course.getCategory());
+        return courseRepository.save(course);
+        //return ResponseEntity.status(HttpStatus.CREATED).body(courseRepository.save(course));
     }
     
 }
